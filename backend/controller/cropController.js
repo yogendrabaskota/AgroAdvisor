fa2a9bdd68eda8ab270c6fac3cf9d78a323e69e3
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-12-11 16:48:03 +0545

    controller bug fix

--- backend/controller/cropController.js
+++ backend/controller/cropController.js
@@ -5,12 +5,17 @@ exports.cropController = async(req, res) => {
     const { N, P, K, temperature, humidity, ph, rainfall } = req.body;
 
     if (!N || !P || !K || !temperature || !humidity || !ph || !rainfall) {
-        return res.status(400).json({ message: 'All fields are required' });
+        return res.status(400).json({ 
+            message: 'please provide  N, P, K, temperature, humidity, ph, rainfall'
+         });
     }
 
     try {
         const recommendedCrop = recommendCrop({ N, P, K, temperature, humidity, ph, rainfall });
-        res.status(200).json({ crop: recommendedCrop });
+        res.status(200).json({ 
+            message : `Best crop for this condition is : ${recommendedCrop}`
+         })
+
     } catch (error) {
         res.status(500).json({ message: 'Error processing request', error });
     }
