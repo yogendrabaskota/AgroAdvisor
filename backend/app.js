6008bf443ae9a971cd0132629d9a522af15d416f
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-12-15 21:51:38 +0545

    live server added

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,13 @@ app.use(bodyParser.json());
 
 app.use('/api/crops', cropRoutes);
 
+app.post("/",(req,res)=>{
+    res.status(200).json({
+        message : "I am here"
+    })
+
+})
+
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- frontend/src/components/CropForm.jsx
+++ frontend/src/components/CropForm.jsx
@@ -24,7 +24,7 @@ const CropForm = () => {
         e.preventDefault();
         setLoading(true);
         try {
-            const response = await axios.post('http://localhost:5000/api/crops/recommend', formData);
+            const response = await axios.post('https://agroadvisor-digi.onrender.com/api/crops/recommend', formData);
             setResult(response.data.crop);
         } catch (error) {
             console.error('Error recommending crop:', error);
